f58e545089803e3dc51826ccd2871d025bd90429
aarohisp <112809842+aarohisp@users.noreply.github.com>
2023-07-24 15:24:21 +0530

    added basic structure logic for node.js
    added basic structure logic for node.js to implement functionalities to interact with database

--- help_hive_project/server.js
+++ help_hive_project/server.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const port = 3000;
 
-const PORT = process.env.PORT || 3000;
+// Middleware to serve static files (CSS, JS, images, etc.)
+app.use(express.static("public"));
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, "public")));
-
-// Define a route for the homepage
+// Home route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(__dirname + "/index.html");
 });
 
+// Search route (Example: handle the search functionality)
+app.get("/search", (req, res) => {
+  const searchQuery = req.query.q; // Get the search query from the URL parameter 'q'
+  // Add your logic here to process the search query and return relevant results
+  // For now, let's just send back the search query as a response
+  res.send(`You searched for: ${searchQuery}`);
+});
+
+// Product description route (Example: handle product description page)
+app.get("/Product_Description", (req, res) => {
+  // Add your logic here to fetch product details based on the request and send the product description page
+  res.send("Product Description Page");
+});
+
+// Handle other routes (if required)
+// ...
+
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
